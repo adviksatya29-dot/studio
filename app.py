0f30fa4aa55df90d021c93bda71bb57f1b49e960
adviksatya29-dot <adviksatya29@gmail.com>
2026-04-02 10:04:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -157,6 +157,8 @@ def logout():
 
 # ---------------- RUN APP ----------------
 import os
+
 if __name__ == "__main__":
+    # Get port from environment variable, default to 10000 if not found
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host='0.0.0.0', port=port)
